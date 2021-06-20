e1cf363ffb511c15d5ee689d2dfcd15ad7ae79be
Trombitas Sandor <tis.sandor@gmail.com>
2021-06-20 10:09:33 +0300

    change(config): remove allTags

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,4 @@ module.exports = function (eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy('images');
   eleventyConfig.addPassthroughCopy('css');
-
-  eleventyConfig.addCollection('allTags', (collectionApi) => {
-    return collectionApi.getAll();
-  });
 };
