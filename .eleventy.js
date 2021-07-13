a31c81761529bdd918d45f724966f073970ae88d
Trombitas Sandor <tis.sandor@gmail.com>
2021-07-13 08:52:20 +0300

    Change: pass css through cssnano via postcss

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 module.exports = function (eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy('images');
-  eleventyConfig.addPassthroughCopy('css');
 };
